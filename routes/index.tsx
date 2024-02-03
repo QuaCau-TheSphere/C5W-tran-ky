f870616269202d78202f8a3ef884e8c367d1808b
ooker <ganuongphap@gmail.com>
2024-02-03 16:24:29 +0700

    thử dùng export default

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,5 @@
-import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-function handler(_req: Request) {
+export function handler(_req: Request) {
   return Response.redirect("https://example.com", 307);
 }
 
-serve(handler);
